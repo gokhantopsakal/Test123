f7f4b8e1afce6db2d938931354204c99fdbc89b5
Numan Karaaslan <numan4634@hotmail.com>
2022-02-09 11:14:02 +0300

    ikinci commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,6 +2,8 @@ public class MainClass
 {
     public static void main(String[] args)
     {
-
+        Person person = new Person();
+        person.setIsim("numan");
+        System.out.println(person.getIsim());
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,14 @@
+public class Person
+{
+    private String isim;
+
+    public String getIsim()
+    {
+        return isim;
+    }
+
+    public void setIsim(String isim)
+    {
+        this.isim = isim;
+    }
+}
